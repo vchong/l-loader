a120b0b1701f7cc2f76885cad43dbdef2f73cca9
Haojian Zhuang <haojian.zhuang@linaro.org>
2017-03-25 16:09:40 +0800

    generate_ptable: compatable with hisi ptable
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- generate_ptable.sh
+++ generate_ptable.sh
@@ -25,8 +25,8 @@ case ${PTABLE} in
   aosp-8g|linux-8g)
     SECTOR_NUMBER=2097152
     ;;
-  aosp-64g|linux-64g)
-    SECTOR_NUMBER=15616000
+  aosp-16g|linux-16g)
+    SECTOR_NUMBER=4194304
     ;;
 esac
 
@@ -38,7 +38,7 @@ case ${PTABLE} in
     fakeroot ${SGDISK} -n 1:2048:4095 -t 1:0700 -u 1:F9F21F01-A8D4-5F0E-9746-594869AEC3E4 -c 1:"vrl" -p ${TEMP_FILE}
     fakeroot ${SGDISK} -n 2:4096:6143 -t 2:0700 -u 2:F9F21F02-A8D4-5F04-9746-594869AEC3E4 -c 2:"vrl_backup" -p ${TEMP_FILE}
     ;;
-  aosp*)
+  aosp-8g|aosp-16g)
     dd if=/dev/zero of=${TEMP_FILE} bs=${SECTOR_SIZE} count=${SECTOR_NUMBER} conv=sparse
     fakeroot sgdisk -U 2CB85345-6A91-4043-8203-723F0D28FBE8 -v ${TEMP_FILE}
     #[1: nvme: 1M-7M]
@@ -46,21 +46,24 @@ case ${PTABLE} in
     #[2: cache: 7M-263M]
     fakeroot ${SGDISK} -n 2:0:+256M -a ${ALIGNMENT} -t 2:8301 -u 2:b28add62-da27-4dd2-8dea-e3628a513929 -c 2:"cache" ${TEMP_FILE}
     #[3: mcuimage: 263M-264M]
-    fakeroot ${SGDISK} -n 3:0:+1M -t 3:0700 -u 3:61A36FC1-8EFB-4899-84D8-B61642EFA723 -c 3:"mcuimage" ${TEMP_FILE}
+    #debug to enlarge 80M for compatible with old partition table from hisilicon
+    fakeroot ${SGDISK} -n 3:0:+81M -t 3:0700 -u 3:61A36FC1-8EFB-4899-84D8-B61642EFA723 -c 3:"mcuimage" ${TEMP_FILE}
     #[4: fastboot: 264M-276M]
     fakeroot ${SGDISK} -n 4:0:+12M -t 4:0700 -u 4:00354BCD-BBCB-4CB3-B5AE-CDEFCB5DAC43 -c 4:"fastboot" ${TEMP_FILE}
     #[5: boot: 276M-340M]
     fakeroot ${SGDISK} -n 5:0:+64M -t 5:EF00 -u 5:61b94129-c1f2-4601-99ea-a1f518e1b082 -c 5:"boot" ${TEMP_FILE}
-    #[6: dtimage: 340M-356M]
-    fakeroot ${SGDISK} -n 6:0:+16M -t 6:0700 -u 6:A092C620-D178-4CA7-B540-C4E26BD6D2E2 -c 6:"dtimage" ${TEMP_FILE}
-    #[7: trustfirmware: 356M-358M]
-    fakeroot ${SGDISK} -n 7:0:+2M -t 7:0700 -u 7:5C0F213C-17E1-4149-88C8-8B50FB4EC70E -c 7:"trustfirmware" ${TEMP_FILE}
-    #[8: system: 358M-5046M]
-    fakeroot ${SGDISK} -n 8:0:+4688M -t 8:8300 -u 8:BED8EBDC-298E-4A7A-B1F1-2500D98453B7 -c 8:"system" ${TEMP_FILE}
-    #[9: vendor: 5046M-5830M]
-    fakeroot ${SGDISK} -n 9:0:+784M -t 9:0700 -u 9:FC56E345-2E8E-49AE-B2F8-5B9D263FE377 -c 9:"vendor" ${TEMP_FILE}
-    #[10: userdata: 5830M-End]
-    fakeroot ${SGDISK} -n -E -t 10:8300 -u 10:064111F6-463B-4CE1-876B-13F3684CE164 -c 10:"userdata" -p -D ${TEMP_FILE}
+    #[6: fip: 340M-352M]
+    fakeroot ${SGDISK} -n 6:0:+12M -t 6:0700 -u 6:17cc2359-dad2-4214-8362-1bfebc9a85fd -c 6:"fip" ${TEMP_FILE}
+    #[7: dtimage: 352M-374M]
+    fakeroot ${SGDISK} -n 7:0:+16M -t 7:0700 -u 7:A092C620-D178-4CA7-B540-C4E26BD6D2E2 -c 7:"dtimage" ${TEMP_FILE}
+    #[8: trustfirmware: 374M-376M]
+    fakeroot ${SGDISK} -n 8:0:+2M -t 8:0700 -u 8:5C0F213C-17E1-4149-88C8-8B50FB4EC70E -c 8:"trustfirmware" ${TEMP_FILE}
+    #[9: system: 376M-5058M]
+    fakeroot ${SGDISK} -n 9:0:+4688M -t 9:8300 -u 9:BED8EBDC-298E-4A7A-B1F1-2500D98453B7 -c 9:"system" ${TEMP_FILE}
+    #[10: vendor: 5058M-5842M]
+    fakeroot ${SGDISK} -n 10:0:+784M -t 10:0700 -u 10:FC56E345-2E8E-49AE-B2F8-5B9D263FE377 -c 10:"vendor" ${TEMP_FILE}
+    #[11: userdata: 5830M-End]
+    fakeroot ${SGDISK} -n -E -t 11:8300 -u 11:064111F6-463B-4CE1-876B-13F3684CE164 -c 11:"userdata" -p -D ${TEMP_FILE}
     ;;
   linux*)
     dd if=/dev/zero of=${TEMP_FILE} bs=${SECTOR_SIZE} count=${SECTOR_NUMBER} conv=sparse
