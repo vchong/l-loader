5786e3c5bc37088ed046c6e83d8cb6ab5ccd3ead
Haojian Zhuang <haojian.zhuang@linaro.org>
2017-04-28 21:32:34 +0800

    hikey960: redefine the ptable
    Since the partition shouldn't be 512KB, abandon to be compatible
    with Hisilicon ptable. Define a new one instead.
    
    And move fip partition to lower address.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- generate_ptable.sh
+++ generate_ptable.sh
@@ -13,7 +13,6 @@ ENTRIES_IN_SECTOR=$(expr ${SECTOR_SIZE} / 128)
 ENTRY_SECTORS=$(expr 128 / ${ENTRIES_IN_SECTOR})
 PRIMARY_SECTORS=$(expr ${ENTRY_SECTORS} + 2)
 SECONDARY_SECTORS=$(expr ${ENTRY_SECTORS} + 1)
-ALIGNMENT=128
 
 case ${SECTOR_SIZE} in
   512)
@@ -57,32 +56,32 @@ case ${PTABLE} in
   aosp-32g|aosp-64g)
     dd if=/dev/zero of=${TEMP_FILE} bs=${SECTOR_SIZE} count=${SECTOR_NUMBER} conv=sparse
     fakeroot sgdisk -U 2CB85345-6A91-4043-8203-723F0D28FBE8 -v ${TEMP_FILE}
-    #[1: xloader_reserved1: 1M-1.5M]
-    fakeroot ${SGDISK} -n 1:0:+512K -a ${ALIGNMENT} -t 1:0700 -u 1:697c41e0-7a59-4dfa-a9a6-aa43ac5be684 -c 1:"xloader_reserved1" ${TEMP_FILE}
-    #[2: xloader_reserved2: 1.5M-2M]
-    fakeroot ${SGDISK} -n 2:0:+512K -a ${ALIGNMENT} -t 2:0700 -u 2:1ede8873-47dc-45d0-b441-e367e6ea3b4a -c 2:"xloader_reserved2" ${TEMP_FILE}
-    #[3: fastboot: 2M-14M]
-    fakeroot ${SGDISK} -n 3:0:+12M -a ${ALIGNMENT} -t 3:0700 -u 3:3f5f8c48-4402-4ace-9058-30bfea4fa53f -c 3:"fastboot" ${TEMP_FILE}
-    #[4: nvme: 14M-20M]
-    fakeroot ${SGDISK} -n 4:0:+6M -a ${ALIGNMENT} -t 4:0700 -u 4:4c7a5919-d512-4d2e-bdd5-1ceb799a1c7e -c 4:"nvme" ${TEMP_FILE}
-    #[5: cache: 20M-276M]
-    fakeroot ${SGDISK} -n 5:0:+256M -a ${ALIGNMENT} -t 5:0700 -u 5:10cc3268-05f0-4db2-aa00-707361427fc8 -c 5:"cache" ${TEMP_FILE}
-    #[6: mcuimage: 276M-277M]
-    fakeroot ${SGDISK} -n 6:0:+1M -a ${ALIGNMENT} -t 6:0700 -u 6:5d8481d4-c170-4aa8-9438-8743c73ea8f5 -c 6:"mcuimage" ${TEMP_FILE}
-    #[7: boot: 277M-341M]
-    fakeroot ${SGDISK} -n 7:0:+64M -a ${ALIGNMENT} -t 7:EF00 -u 7:d3340696-9b95-4c64-8df6-e6d4548fba41 -c 7:"boot" ${TEMP_FILE}
-    #[8: dts: 341M-357M]
-    fakeroot ${SGDISK} -n 8:0:+16M -a ${ALIGNMENT} -t 8:0700 -u 8:6e53b0bb-fa7e-4206-b607-5ae699e9f066 -c 8:"dts" ${TEMP_FILE}
-    #[9: trustfirmware: 357M-359M]
-    fakeroot ${SGDISK} -n 9:0:+2M -a ${ALIGNMENT} -t 9:0700 -u 9:611eac6b-bc42-4d72-90ac-418569c8e9b8 -c 9:"trustfirmware" ${TEMP_FILE}
-    #[10: system: 359M-5047M]
-    fakeroot ${SGDISK} -n 10:0:+4688M -a ${ALIGNMENT} -t 10:8300 -u 10:c3e50923-fb85-4153-b925-759614d4dfcd -c 10:"system" ${TEMP_FILE}
-    #[11: vendor: 5047M-5831M]
-    fakeroot ${SGDISK} -n 11:0:+784M -a ${ALIGNMENT} -t 11:0700 -u 11:919d7080-d71a-4ae1-9227-e4585210c837 -c 11:"vendor" ${TEMP_FILE}
-    #[12: fip: 5831M-5843M]
-    fakeroot ${SGDISK} -n 12:0:+12M -a ${ALIGNMENT} -t 12:0700 -u 12:2ff70fee-cbdd-4cfa-b100-d8b0bb271a5c -c 12:"fip" ${TEMP_FILE}
-    #[13: userdata: 5843M-End]
-    fakeroot ${SGDISK} -n -E -t 13:8300 -u 13:049b9a32-a36a-483e-ab6f-9ef6644e6d47 -c 13:"userdata" ${TEMP_FILE}
+    # Hisilicon creates 2 xloader partitions which are 512KB size. All partitions should be aligned at least 1MB with sgdisk.
+    # So dicard one xloader partition in this tool.
+    #[1: xloader: 1M-2M]
+    fakeroot ${SGDISK} -n 1:0:+1M -t 1:0700 -u 1:697c41e0-7a59-4dfa-a9a6-aa43ac5be684 -c 1:"xloader" ${TEMP_FILE}
+    #[2: fastboot: 2M-14M]
+    fakeroot ${SGDISK} -n 2:0:+12M -t 2:0700 -u 2:3f5f8c48-4402-4ace-9058-30bfea4fa53f -c 2:"fastboot" ${TEMP_FILE}
+    #[3: fip: 14M-26M]
+    fakeroot ${SGDISK} -n 3:0:+12M -t 3:0700 -u 3:dc1a888e-f17c-4964-92d6-f8fcc402ed8b -c 3:"fip" ${TEMP_FILE}
+    #[4: nvme: 26M-32M]
+    fakeroot ${SGDISK} -n 4:0:+6M -t 4:0700 -u 4:4c7a5919-d512-4d2e-bdd5-1ceb799a1c7e -c 4:"nvme" ${TEMP_FILE}
+    #[5: dts: 32M-64M]
+    fakeroot ${SGDISK} -n 5:0:+32M -t 5:0700 -u 5:6e53b0bb-fa7e-4206-b607-5ae699e9f066 -c 5:"dts" ${TEMP_FILE}
+    #[6: boot: 64M-128M]
+    fakeroot ${SGDISK} -n 6:0:+64M -t 6:EF00 -u 6:d3340696-9b95-4c64-8df6-e6d4548fba41 -c 6:"boot" ${TEMP_FILE}
+    #[7: reserved: 128M-512M]
+    fakeroot ${SGDISK} -n 7:0:+384M -t 7:0700 -u 7:611eac6b-bc42-4d72-90ac-418569c8e9b8 -c 7:"reserved" ${TEMP_FILE}
+    #[8: cache: 512M-768M]
+    fakeroot ${SGDISK} -n 8:0:+256M -t 8:0700 -u 8:10cc3268-05f0-4db2-aa00-707361427fc8 -c 8:"cache" ${TEMP_FILE}
+    #[9: mcuimage: 768M-776M]
+    fakeroot ${SGDISK} -n 9:0:+8M -t 9:0700 -u 9:5d8481d4-c170-4aa8-9438-8743c73ea8f5 -c 9:"mcuimage" ${TEMP_FILE}
+    #[10: system: 776M-5464M]
+    fakeroot ${SGDISK} -n 10:0:+4688M -t 10:8300 -u 10:c3e50923-fb85-4153-b925-759614d4dfcd -c 10:"system" ${TEMP_FILE}
+    #[11: vendor: 5464M-6248M]
+    fakeroot ${SGDISK} -n 11:0:+784M -t 11:0700 -u 11:919d7080-d71a-4ae1-9227-e4585210c837 -c 11:"vendor" ${TEMP_FILE}
+    #[12: userdata: 6248M-End]
+    fakeroot ${SGDISK} -n -E -t 12:8300 -u 12:049b9a32-a36a-483e-ab6f-9ef6644e6d47 -c 12:"userdata" ${TEMP_FILE}
     ;;
   linux*)
     dd if=/dev/zero of=${TEMP_FILE} bs=${SECTOR_SIZE} count=${SECTOR_NUMBER} conv=sparse
