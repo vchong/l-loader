781ad33d88f1b6417032c5f5e2dad45dae3905c6
Victor Chong <victor.chong@linaro.org>
2017-09-26 00:58:49 +0100

    Create and use makefiles
    Rather than running each build instructions manually
    
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

---
+++ hikey960.mk
@@ -0,0 +1,18 @@
+BL1=bl1.bin
+NS_BL1U=BL33_AP_UEFI.fd
+PTABLE_LST=aosp-32g
+
+.PHONY: all
+all: l-loader.bin prm_ptable.img
+
+l-loader.bin: $(BL1) $(NS_BL1U)
+	python gen_loader_hikey960.py -o $@ --img_bl1=$(BL1) --img_ns_bl1u=$(NS_BL1U)
+
+prm_ptable.img:
+	for ptable in $(PTABLE_LST); do \
+		PTABLE=$${ptable} SECTOR_SIZE=4096 SGDISK=./sgdisk bash -x generate_ptable.sh;\
+	done
+
+.PHONY: clean
+clean:
+	rm -f *.img l-loader.bin
