d4d4bcb8e515ad1d7ca50052ee16e9d60411fac7
Victor Chong <victor.chong@linaro.org>
2017-10-25 15:11:11 +0100

    hikey960.mk: fix ptable list
    PTABLE_LST is meant to be a list of partition table to generate.
    There's 2 issues:
    * PTABLE_LST is hardcoded and can't be overriden
    * generate_ptable.sh script generate a unique file named prm_ptable.img,
      hence overwrite it if called multiple times
    
    This patch fixes both issues by allowing to override PTABLE_LIST default
    and saving the current copy of prm_table.img to another appropriate
    filename.
    
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- hikey960.mk
+++ hikey960.mk
@@ -1,6 +1,6 @@
 BL1=bl1.bin
 NS_BL1U=BL33_AP_UEFI.fd
-PTABLE_LST=aosp-32g
+PTABLE_LST?=aosp-32g
 
 .PHONY: all
 all: l-loader.bin prm_ptable.img
@@ -11,6 +11,7 @@ l-loader.bin: $(BL1) $(NS_BL1U)
 prm_ptable.img:
 	for ptable in $(PTABLE_LST); do \
 		PTABLE=$${ptable} SECTOR_SIZE=4096 SGDISK=./sgdisk bash -x generate_ptable.sh;\
+		cp prm_ptable.img ptable-$${ptable}.img;\
 	done
 
 .PHONY: clean
