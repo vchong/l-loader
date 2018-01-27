d1980d1c690dcb5004f3bba507b282f232aca004
Haojian Zhuang <haojian.zhuang@linaro.org>
2018-01-27 13:27:34 +0800

    build: migrate to bl2_el3
    Since BL2_EL3 is enabled for non-TF ROM, the output binaries are
    updated in below.
    
    HiKey:
      Recovery Mode (recovery.bin)
      Normal Boot Mode (l-loader.bin --> loader partition,
    		    fip.bin --> fastboot partition)
    
    HiKey960:
      Recovery Mode (recovery.bin)
      Normal Boot Mode (l-loader.bin --> fastboot partition,
    		    fip.bin --> fip partition)
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- hikey960.mk
+++ hikey960.mk
@@ -1,12 +1,13 @@
 BL1=bl1.bin
+BL2=bl2.bin
 NS_BL1U=BL33_AP_UEFI.fd
 PTABLE_LST:=aosp-32g aosp-32g-spare linux-32g
 
 .PHONY: all
-all: l-loader.bin prm_ptable.img
+all: l-loader.bin prm_ptable.img recovery.bin
 
-l-loader.bin: $(BL1) $(NS_BL1U)
-	python gen_loader_hikey960.py -o $@ --img_bl1=$(BL1) --img_ns_bl1u=$(NS_BL1U)
+l-loader.bin: $(BL2)
+	cp -f $(BL2) l-loader.bin
 
 prm_ptable.img:
 	for ptable in $(PTABLE_LST); do \
@@ -14,6 +15,9 @@ prm_ptable.img:
 		cp prm_ptable.img ptable-$${ptable}.img;\
 	done
 
+recovery.bin: $(BL1) $(NS_BL1U)
+	python gen_loader_hikey960.py -o $@ --img_bl1=$(BL1) --img_ns_bl1u=$(NS_BL1U)
+
 .PHONY: clean
 clean:
-	rm -f *.img l-loader.bin
+	rm -f prm_ptable.img l-loader.bin recovery.bin
