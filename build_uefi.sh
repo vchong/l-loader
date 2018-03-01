07bbcf20682c7572ed834f83201bd3d15238411a
Haojian Zhuang <haojian.zhuang@linaro.org>
2018-03-01 23:06:31 +0800

    build_uefi.sh: support to build edk2-platforms
    1. Support to build edk2, edk2-platforms & edk2-non-osi. These
    are upstream version of edk2.
    2. Support CLANG 3.9. OPTEE couldn't be built by CLANG yet because
    of thumb instructions.
    3. Use bash to run this script.
    $bash build_uefi.sh <platform>
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- build_uefi.sh
+++ build_uefi.sh
@@ -1,54 +1,46 @@
 #!/bin/sh
+#
+# Usage: bash build_uefi.sh {platform}
+#
+
 #BUILD_OPTION=DEBUG
 BUILD_OPTION=RELEASE
-AARCH64_GCC=LINARO_GCC_7_1    # Prefer to use Linaro GCC 7.1.1. Otherwise, user may meet some toolchain issues.
+#AARCH64_GCC=CLANG_3_9
+AARCH64_GCC=LINARO_GCC_7_2    # Prefer to use Linaro GCC >= 7.1.1. Otherwise, user may meet some toolchain issues.
 #GENERATE_PTABLE=1
+#EDK2_PLATFORM=1
+
+# l-loader on hikey and optee need AARCH32_GCC
+AARCH32_GCC=/opt/toolchain/gcc-linaro-arm-linux-gnueabihf-4.8-2014.01_linux/bin/
+PATH=${AARCH32_GCC}:${PATH} && export PATH
 
 # Setup environment variables that are used in uefi-tools
-AARCH32_GCC=/opt/toolchain/gcc-linaro-arm-linux-gnueabihf-4.8-2014.01_linux/bin/
-
 case "${AARCH64_GCC}" in
 "ARNDROID_GCC_4_9")
 	AARCH64_GCC_4_9=/opt/toolchain/aarch64-linux-android-4.9.git/bin/
 	PATH=${AARCH64_GCC_4_9}:${PATH} && export PATH
 	export AARCH64_TOOLCHAIN=GCC49
 	CROSS_COMPILE=aarch64-linux-android-
-	;;
-"LINARO_GCC_4_8")
-	AARCH64_GCC_4_8=/opt/toolchain/gcc-linaro-aarch64-linux-gnu-4.8-2014.04_linux/bin/
-	PATH=${AARCH64_GCC_4_8}:${PATH} && export PATH
-	export AARCH64_TOOLCHAIN=GCC48
-	CROSS_COMPILE=aarch64-linux-gnu-
-	;;
-"LINARO_GCC_4_9")
-	AARCH64_GCC_4_9=/opt/toolchain/gcc-linaro-4.9-2014.11-x86_64_aarch64-linux-gnu/bin/
-	PATH=${AARCH64_GCC_4_9}:${PATH} && export PATH
-	export AARCH64_TOOLCHAIN=GCC49
-	CROSS_COMPILE=aarch64-linux-gnu-
-	;;
-"LINARO_GCC_5_3")
-	AARCH64_GCC_5_3=/opt/toolchain/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu/bin/
-	PATH=${AARCH64_GCC_5_3}:${PATH} && export PATH
-	export AARCH64_TOOLCHAIN=GCC5
-	CROSS_COMPILE=aarch64-linux-gnu-
-	;;
-"LINARO_GCC_5_4")
-	AARCH64_GCC_5_4=/opt/toolchain/gcc-linaro-5.4.1-2017.05-i686_aarch64-linux-gnu/bin/
-	PATH=${AARCH64_GCC_5_4}:${PATH} && export PATH
-	export AARCH64_TOOLCHAIN=GCC5
-	CROSS_COMPILE=aarch64-linux-gnu-
-	;;
-"LINARO_GCC_6_4")
-	AARCH64_GCC_6_4=/opt/toolchain/gcc-linaro-6.4.1-2017.08-x86_64_aarch64-linux-gnu/bin/
-	PATH=${AARCH64_GCC_6_4}:${PATH} && export PATH
-	export AARCH64_TOOLCHAIN=GCC5
-	CROSS_COMPILE=aarch64-linux-gnu-
+	TOOLCHAIN_FAMILY=gcc
 	;;
 "LINARO_GCC_7_1")
 	AARCH64_GCC_7_1=/opt/toolchain/gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu/bin/
 	PATH=${AARCH64_GCC_7_1}:${PATH} && export PATH
 	export AARCH64_TOOLCHAIN=GCC5
 	CROSS_COMPILE=aarch64-linux-gnu-
+	TOOLCHAIN_FAMILY=gcc
+	;;
+"LINARO_GCC_7_2")
+	AARCH64_GCC_7_2=/opt/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/
+	PATH=${AARCH64_GCC_7_2}:${PATH} && export PATH
+	export AARCH64_TOOLCHAIN=GCC5
+	CROSS_COMPILE=aarch64-linux-gnu-
+	TOOLCHAIN_FAMILY=gcc
+	;;
+"CLANG_3_9")
+	export AARCH64_TOOLCHAIN=CLANG38
+	export CC=/usr/bin/clang
+	TOOLCHAIN_FAMILY=clang
 	;;
 *)
 	echo "Not supported toolchain:${AARCH64_GCC}"
@@ -100,11 +92,19 @@ case "$PLATFORM" in
 		echo "Warning: Can't find fastboot source code to build"
 		exit
 	fi
-	EDK2_OUTPUT_DIR=${EDK2_DIR}/Build/HiKey/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
+	if [ $EDK2_PLATFORM ]; then
+		EDK2_OUTPUT_DIR=${BUILD_PATH}/l-loader/Build/HiKey/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
+	else
+		EDK2_OUTPUT_DIR=${EDK2_DIR}/Build/HiKey/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
+	fi
 	cd ${BUILD_PATH}
 	;;
 "hikey960")
-	EDK2_OUTPUT_DIR=${EDK2_DIR}/Build/HiKey960/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
+	if [ $EDK2_PLATFORM ]; then
+		EDK2_OUTPUT_DIR=${BUILD_PATH}/l-loader/Build/HiKey960/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
+	else
+		EDK2_OUTPUT_DIR=${EDK2_DIR}/Build/HiKey960/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
+	fi
 	cd ${BUILD_PATH}
 	;;
 esac
@@ -145,16 +145,26 @@ esac
 case "${PLATFORM}" in
 "hikey")
 	cd ${BUILD_PATH}/atf-fastboot
-	CROSS_COMPILE=${CROSS_COMPILE} make PLAT=${PLATFORM} DEBUG=${BUILD_DEBUG}
+	case "${TOOLCHAIN_FAMILY}" in
+	"gcc")
+		CROSS_COMPILE=aarch64-linux-gnu- make PLAT=${PLATFORM} DEBUG=${BUILD_DEBUG}
+		;;
+	"clang")
+		CROSS_COMPILE=aarch64-linux-gnu- PATH=/opt/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/:${PATH} make CC=/usr/bin/clang PLAT=${PLATFORM} DEBUG=${BUILD_DEBUG}
+		;;
+	*)
+		echo "Invalid toolchain family: ${TOOLCHAIN_FAMILY}"
+		exit
+		;;
+	esac
 	if [ $? != 0 ]; then
 		echo "Fail to build fastboot ($?)"
 		exit
 	fi
 	# Convert "DEBUG"/"RELEASE" to "debug"/"release"
-	FASTBOOT_BUILD_OPTION=$(echo ${BUILD_OPTION} | tr '[A-Z]' '[a-z]')
-	if [ -f ${BUILD_PATH}/atf-fastboot/build/${PLATFORM}/${FASTBOOT_BUILD_OPTION}/bl1.bin ]; then
+	if [ -f ${BUILD_PATH}/atf-fastboot/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/bl1.bin ]; then
 		cd ${BUILD_PATH}/l-loader
-		ln -sf ${BUILD_PATH}/atf-fastboot/build/${PLATFORM}/${FASTBOOT_BUILD_OPTION}/bl1.bin fastboot.bin
+		ln -sf ${BUILD_PATH}/atf-fastboot/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/bl1.bin fastboot.bin
 	else
 		echo "ERROR: Can't find fastboot binary"
 		exit
@@ -162,27 +172,60 @@ case "${PLATFORM}" in
 	;;
 esac
 
+function do_symlink()
+{
+	# Locate output files of UEFI & Arm Trust Firmware
+	cd ${BUILD_PATH}/l-loader
+	if [ $EDK2_PLATFORM ]; then
+		echo "BUILD_OPTION:$BUILD_OPTION"
+		ln -sf ../arm-trusted-firmware/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/bl1.bin
+		ln -sf ../arm-trusted-firmware/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/bl2.bin
+		ln -sf ../arm-trusted-firmware/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/fip.bin
+	else
+		ln -sf ${EDK2_OUTPUT_DIR}/FV/bl1.bin
+		ln -sf ${EDK2_OUTPUT_DIR}/FV/bl2.bin
+		ln -sf ${EDK2_OUTPUT_DIR}/FV/fip.bin
+	fi
+	if [ -f ${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd ]; then
+		ln -sf ${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd
+	fi
+}
+
 # Build UEFI & ARM Trusted Firmware
-cd ${EDK2_DIR}
-#${UEFI_TOOLS_DIR}/uefi-build.sh -b $BUILD_OPTION -a ../arm-trusted-firmware -s ../optee_os $PLATFORM
-${UEFI_TOOLS_DIR}/uefi-build.sh -b $BUILD_OPTION -a ../arm-trusted-firmware $PLATFORM
+if [ $EDK2_PLATFORM ]; then
+	# Must not build in edk2 directory. Otherwise, we'll meet build failure.
+	cd ${BUILD_PATH}/l-loader
+	case "${TOOLCHAIN_FAMILY}" in
+	"gcc")
+		${UEFI_TOOLS_DIR}/edk2-build.sh -b $BUILD_OPTION -a ../arm-trusted-firmware -s ../optee_os -e ../edk2 -p ../edk2-platforms -n ../edk2-non-osi -T $TOOLCHAIN_FAMILY $PLATFORM
+		;;
+	"clang")
+		# Build edk2
+		${UEFI_TOOLS_DIR}/edk2-build.sh -b $BUILD_OPTION -e ../edk2 -p ../edk2-platforms -n ../edk2-non-osi -T $TOOLCHAIN_FAMILY $PLATFORM
+		# Build OPTEE
+		# Can't support thumb
+		#cd ${BUILD_PATH}/optee_os
+		#CROSS_COMPILE=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- PATH=/opt/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/:${PATH} make CC=/usr/bin/clang PLATFORM=hikey-hikey960 CFG_ARM_core=y
+		# Build ARM Trusted Firmware
+		cd ${BUILD_PATH}/arm-trusted-firmware
+		#CROSS_COMPILE=aarch64-linux-gnu- PATH=/opt/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/:${PATH} make CC=/usr/bin/clang PLAT=$PLATFORM SPD=opteed BL32=../optee_os/out/arm-plat-hikey/core/tee-pager.bin BL33=${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd DEBUG=$BUILD_DEBUG all
+		CROSS_COMPILE=aarch64-linux-gnu- PATH=/opt/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/:${PATH} make CC=/usr/bin/clang PLAT=$PLATFORM BL33=${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd DEBUG=$BUILD_DEBUG all fip
+		;;
+	esac
+else
+	cd ${EDK2_DIR}
+	${UEFI_TOOLS_DIR}/uefi-build.sh -b $BUILD_OPTION -a ../arm-trusted-firmware $PLATFORM
+	#${UEFI_TOOLS_DIR}/uefi-build.sh -b $BUILD_OPTION -a ../arm-trusted-firmware -s ../optee_os $PLATFORM
+fi
 if [ $? != 0 ]; then
 	echo "Fail to build UEFI & ARM Trusted Firmware ($?)"
 	exit
 fi
 
-# Locate output files of UEFI & Arm Trust Firmware
-cd ${BUILD_PATH}/l-loader
-ln -sf ${EDK2_OUTPUT_DIR}/FV/bl1.bin
-ln -sf ${EDK2_OUTPUT_DIR}/FV/bl2.bin
-ln -sf ${EDK2_OUTPUT_DIR}/FV/fip.bin
-if [ -f ${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd ]; then
-	ln -sf ${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd
-fi
+do_symlink
 
 case "${PLATFORM}" in
 "hikey")
-	PATH=${AARCH32_GCC}:${PATH} && export PATH
 	# Patch aarch64 mode on bl1.bin. Then bind it with fastboot.
 	make -f ${PLATFORM}.mk recovery.bin
 	# Patch aarch64 mode on bl2.bin
