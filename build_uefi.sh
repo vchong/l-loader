5373d3aacb9209bee25c0352ab4ede40e4e9e3ae
Haojian Zhuang <haojian.zhuang@linaro.org>
2018-03-02 17:07:47 +0800

    build_uefi.sh: fix to build optee
    1. The parameter to build optee is wrong. It always builds
    optee for hikey960 even when we want to build hikey.
    2. Change the path of Build directory.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- build_uefi.sh
+++ build_uefi.sh
@@ -100,7 +100,7 @@ case "$PLATFORM" in
 		exit
 	fi
 	if [ $EDK2_PLATFORM ]; then
-		EDK2_OUTPUT_DIR=${BUILD_PATH}/l-loader/Build/HiKey/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
+		EDK2_OUTPUT_DIR=${BUILD_PATH}/Build/HiKey/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
 	else
 		EDK2_OUTPUT_DIR=${EDK2_DIR}/Build/HiKey/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
 	fi
@@ -108,7 +108,7 @@ case "$PLATFORM" in
 	;;
 "hikey960")
 	if [ $EDK2_PLATFORM ]; then
-		EDK2_OUTPUT_DIR=${BUILD_PATH}/l-loader/Build/HiKey960/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
+		EDK2_OUTPUT_DIR=${BUILD_PATH}/Build/HiKey960/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
 	else
 		EDK2_OUTPUT_DIR=${EDK2_DIR}/Build/HiKey960/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
 	fi
@@ -125,6 +125,7 @@ rm -fr ${BUILD_PATH}/arm-trusted-firmware/build
 rm -fr ${BUILD_PATH}/atf-fastboot/build
 cd ${EDK2_DIR}/BaseTools
 make clean
+rm -fr ${BUILD_PATH}/Build/
 rm -fr ${EDK2_DIR}/Build/
 rm -f ${EDK2_OUTPUT_DIR}/FV/bl1.bin
 rm -f ${EDK2_OUTPUT_DIR}/FV/fip.bin
@@ -194,30 +195,36 @@ function do_symlink()
 function do_build()
 {
 	# Build edk2
-	${UEFI_TOOLS_DIR}/edk2-build.sh -b $BUILD_OPTION -e ../edk2 -p ../edk2-platforms -n ../edk2-non-osi -T clang $PLATFORM
-	# Build OPTEE
-	cd ${BUILD_PATH}/optee_os
-	CROSS_COMPILE=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- PATH=${AARCH64_GCC}:${PATH} make PLATFORM=hikey-hikey960 CFG_ARM64_core=y
-	# Build ARM Trusted Firmware
-	cd ${BUILD_PATH}/arm-trusted-firmware
+	cd ${BUILD_PATH}
+	export WORKSPACE=${BUILD_PATH}
+	export PACKAGES_PATH=${WORKSPACE}/edk2:${WORKSPACE}/edk2-platforms:${WORKSPACE}/edk2-non-osi
+	${UEFI_TOOLS_DIR}/edk2-build.sh -b $BUILD_OPTION -e edk2 -p edk2-platforms -n edk2-non-osi -T clang $PLATFORM
 	case "${PLATFORM}" in
 	"hikey")
+		DSC=Platform/Hisilicon/HiKey/HiKey.dsc
 		SCP_BL2=../edk2-non-osi/Platform/Hisilicon/HiKey/mcuimage.bin
 		;;
 	"hikey960")
+		DSC=Platform/Hisilicon/HiKey960/HiKey960.dsc
 		SCP_BL2=../edk2-non-osi/Platform/Hisilicon/HiKey960/lpm3.img
 		;;
 	esac
+	#. edk2/edksetup.sh
+	#build -a AARCH64 -t CLANG38 -p $DSC -b $BUILD_OPTION
+	# Build OPTEE
+	cd ${BUILD_PATH}/optee_os
+	CROSS_COMPILE=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- PATH=${AARCH64_GCC}:${PATH} make PLATFORM=hikey-${PLATFORM} CFG_ARM64_core=y
+	# Build ARM Trusted Firmware
+	cd ${BUILD_PATH}/arm-trusted-firmware
 	BL32=../optee_os/out/arm-plat-hikey/core/tee-pager.bin
 	BL33=${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd
-	#CROSS_COMPILE=aarch64-linux-gnu- PATH=${AARCH64_GCC}:${PATH} make PLAT=$PLATFORM SCP_BL2=$SCP_BL2 SPD=opteed BL32=$BL32 BL33=$BL33 DEBUG=$BUILD_DEBUG all fip
-	CROSS_COMPILE=aarch64-linux-gnu- PATH=${AARCH64_GCC}:${PATH} make PLAT=$PLATFORM SCP_BL2=$SCP_BL2 BL33=$BL33 DEBUG=$BUILD_DEBUG all fip
+	echo "BUILD_DEBUG:${BUILD_DEBUG}"
+	CROSS_COMPILE=aarch64-linux-gnu- PATH=${AARCH64_GCC}:${PATH} make PLAT=${PLATFORM} SCP_BL2=${SCP_BL2} SPD=opteed BL32=${BL32} BL33=${BL33} DEBUG=${BUILD_DEBUG} all fip
 }
 
 # Build UEFI & ARM Trusted Firmware
 if [ $EDK2_PLATFORM ]; then
 	# Must not build in edk2 directory. Otherwise, we'll meet build failure.
-	cd ${BUILD_PATH}/l-loader
 	if [ $CLANG ]; then
 		do_build
 	else
