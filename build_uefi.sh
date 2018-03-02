47dd278d5b3309e46015da7f26d6dcda392cf0c0
Haojian Zhuang <haojian.zhuang@linaro.org>
2018-03-02 17:14:16 +0800

    build_uefi.sh: build edk2 manually
    Build edk2 manually. Tested with four conditions in below.
    
    1. CLANG + edk2-platforms. (successful)
    2. GCC + edk2-platforms. (successful)
    3. CLANG + OpenPlatformPkg. (failure)
    4. GCC + OpenPlatformPkg (successful)
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- build_uefi.sh
+++ build_uefi.sh
@@ -6,9 +6,9 @@
 BUILD_OPTION=DEBUG
 #BUILD_OPTION=RELEASE
 AARCH64_GCC=LINARO_GCC_7_2    # Prefer to use Linaro GCC >= 7.1.1. Otherwise, user may meet some toolchain issues.
-CLANG=CLANG_5_0               # Prefer to use CLANG >= 3.9. Since LLVMgold.so is missing in CLANG 3.8.
+#CLANG=CLANG_5_0               # Prefer to use CLANG >= 3.9. Since LLVMgold.so is missing in CLANG 3.8.
 #GENERATE_PTABLE=1
-EDK2_PLATFORM=1
+#EDK2_PLATFORM=1
 
 # l-loader on hikey and optee need AARCH32_GCC
 AARCH32_GCC=/opt/toolchain/gcc-linaro-arm-linux-gnueabihf-4.8-2014.01_linux/bin/
@@ -49,6 +49,10 @@ case "${CLANG}" in
 	export AARCH64_TOOLCHAIN=CLANG38
 	export CC=/usr/bin/clang
 	;;
+"")
+	# CLANG is not used.
+	export GCC5_AARCH64_PREFIX=aarch64-linux-gnu-
+	;;
 *)
 	echo "Not supported CLANG:${CLANG}"
 	exit
@@ -72,11 +76,11 @@ case "$1" in
 	;;
 esac
 
-if [ -d "${PWD}/edk2" ] && [ -d "${PWD}/uefi-tools" ] && [ -d "${PWD}/arm-trusted-firmware" ] && [ -d "${PWD}/l-loader" ]; then
+if [ -d "${PWD}/edk2" ] && [ -d "${PWD}/arm-trusted-firmware" ] && [ -d "${PWD}/l-loader" ]; then
 	# Check whether source code are available in ${PWD}
 	BUILD_PATH=${PWD}
 	echo "Find source code in ${PWD}"
-elif [ -d "${PWD}/../edk2" ] && [ -d "${PWD}/../uefi-tools" ] && [ -d "${PWD}/../arm-trusted-firmware" ] && [ -d "${PWD}/../l-loader" ]; then
+elif [ -d "${PWD}/../edk2" ] && [ -d "${PWD}/../arm-trusted-firmware" ] && [ -d "${PWD}/../l-loader" ]; then
 	# Check whether source code are available in parent of ${PWD}
 	BUILD_PATH=$(dirname ${PWD})
 	echo "Find source code in parent directory of ${PWD}"
@@ -84,9 +88,6 @@ else
 	echo "Warning: Can't find source code to build."
 	exit
 fi
-
-# Setup environment variables that are used in uefi-tools
-export UEFI_TOOLS_DIR=${BUILD_PATH}/uefi-tools
 
 EDK2_DIR=${BUILD_PATH}/edk2
 echo "edk2 dir:${EDK2_DIR}"
@@ -99,25 +100,12 @@ case "$PLATFORM" in
 		echo "Warning: Can't find fastboot source code to build"
 		exit
 	fi
-	if [ $EDK2_PLATFORM ]; then
-		EDK2_OUTPUT_DIR=${BUILD_PATH}/Build/HiKey/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
-	else
-		EDK2_OUTPUT_DIR=${EDK2_DIR}/Build/HiKey/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
-	fi
-	cd ${BUILD_PATH}
+	EDK2_OUTPUT_DIR=${BUILD_PATH}/Build/HiKey/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
 	;;
 "hikey960")
-	if [ $EDK2_PLATFORM ]; then
-		EDK2_OUTPUT_DIR=${BUILD_PATH}/Build/HiKey960/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
-	else
-		EDK2_OUTPUT_DIR=${EDK2_DIR}/Build/HiKey960/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
-	fi
-	cd ${BUILD_PATH}
+	EDK2_OUTPUT_DIR=${BUILD_PATH}/Build/HiKey960/${BUILD_OPTION}_${AARCH64_TOOLCHAIN}
 	;;
 esac
-
-# Fip.bin is produced in ${EDK2_OUTPUT_DIR}. And ${EDK2_OUTPUT_DIR} is local environment variable.
-echo $EDK2_OUTPUT_DIR
 
 # Always clean build EDK2
 rm -f ${BUILD_PATH}/l-loader/l-loader.bin
@@ -177,16 +165,9 @@ function do_symlink()
 {
 	# Locate output files of UEFI & Arm Trust Firmware
 	cd ${BUILD_PATH}/l-loader
-	if [ $EDK2_PLATFORM ]; then
-		echo "BUILD_OPTION:$BUILD_OPTION"
-		ln -sf ../arm-trusted-firmware/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/bl1.bin
-		ln -sf ../arm-trusted-firmware/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/bl2.bin
-		ln -sf ../arm-trusted-firmware/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/fip.bin
-	else
-		ln -sf ${EDK2_OUTPUT_DIR}/FV/bl1.bin
-		ln -sf ${EDK2_OUTPUT_DIR}/FV/bl2.bin
-		ln -sf ${EDK2_OUTPUT_DIR}/FV/fip.bin
-	fi
+	ln -sf ../arm-trusted-firmware/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/bl1.bin
+	ln -sf ../arm-trusted-firmware/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/bl2.bin
+	ln -sf ../arm-trusted-firmware/build/${PLATFORM}/$(echo ${BUILD_OPTION,,})/fip.bin
 	if [ -f ${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd ]; then
 		ln -sf ${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd
 	fi
@@ -197,20 +178,34 @@ function do_build()
 	# Build edk2
 	cd ${BUILD_PATH}
 	export WORKSPACE=${BUILD_PATH}
-	export PACKAGES_PATH=${WORKSPACE}/edk2:${WORKSPACE}/edk2-platforms:${WORKSPACE}/edk2-non-osi
-	${UEFI_TOOLS_DIR}/edk2-build.sh -b $BUILD_OPTION -e edk2 -p edk2-platforms -n edk2-non-osi -T clang $PLATFORM
-	case "${PLATFORM}" in
-	"hikey")
-		DSC=Platform/Hisilicon/HiKey/HiKey.dsc
-		SCP_BL2=../edk2-non-osi/Platform/Hisilicon/HiKey/mcuimage.bin
-		;;
-	"hikey960")
-		DSC=Platform/Hisilicon/HiKey960/HiKey960.dsc
-		SCP_BL2=../edk2-non-osi/Platform/Hisilicon/HiKey960/lpm3.img
-		;;
-	esac
-	#. edk2/edksetup.sh
-	#build -a AARCH64 -t CLANG38 -p $DSC -b $BUILD_OPTION
+	if [ $EDK2_PLATFORM ]; then
+		export PACKAGES_PATH=${WORKSPACE}/edk2:${WORKSPACE}/edk2-platforms:${WORKSPACE}/edk2-non-osi
+		case "${PLATFORM}" in
+		"hikey")
+			DSC=Platform/Hisilicon/HiKey/HiKey.dsc
+			SCP_BL2=../edk2-non-osi/Platform/Hisilicon/HiKey/mcuimage.bin
+			;;
+		"hikey960")
+			DSC=Platform/Hisilicon/HiKey960/HiKey960.dsc
+			SCP_BL2=../edk2-non-osi/Platform/Hisilicon/HiKey960/lpm3.img
+			;;
+		esac
+	else
+		export PACKAGES_PATH=${WORKSPACE}/edk2
+		case "${PLATFORM}" in
+		"hikey")
+			DSC=OpenPlatformPkg/Platforms/Hisilicon/HiKey/HiKey.dsc
+			SCP_BL2=../edk2/OpenPlatformPkg/Platforms/Hisilicon/HiKey/Binary/mcuimage.bin
+			;;
+		"hikey960")
+			DSC=OpenPlatformPkg/Platforms/Hisilicon/HiKey960/HiKey960.dsc
+			SCP_BL2=../edk2/OpenPlatformPkg/Platforms/Hisilicon/HiKey960/Binary/lpm3.img
+			;;
+		esac
+	fi
+	source edk2/edksetup.sh
+	make -C edk2/BaseTools
+	build -a AARCH64 -t ${AARCH64_TOOLCHAIN} -p ${DSC} -b ${BUILD_OPTION}
 	# Build OPTEE
 	cd ${BUILD_PATH}/optee_os
 	CROSS_COMPILE=arm-linux-gnueabihf- CROSS_COMPILE_core=aarch64-linux-gnu- CROSS_COMPILE_ta_arm64=aarch64-linux-gnu- CROSS_COMPILE_ta_arm32=arm-linux-gnueabihf- PATH=${AARCH64_GCC}:${PATH} make PLATFORM=hikey-${PLATFORM} CFG_ARM64_core=y
@@ -218,22 +213,11 @@ function do_build()
 	cd ${BUILD_PATH}/arm-trusted-firmware
 	BL32=../optee_os/out/arm-plat-hikey/core/tee-pager.bin
 	BL33=${EDK2_OUTPUT_DIR}/FV/BL33_AP_UEFI.fd
-	echo "BUILD_DEBUG:${BUILD_DEBUG}"
 	CROSS_COMPILE=aarch64-linux-gnu- PATH=${AARCH64_GCC}:${PATH} make PLAT=${PLATFORM} SCP_BL2=${SCP_BL2} SPD=opteed BL32=${BL32} BL33=${BL33} DEBUG=${BUILD_DEBUG} all fip
 }
 
 # Build UEFI & ARM Trusted Firmware
-if [ $EDK2_PLATFORM ]; then
-	# Must not build in edk2 directory. Otherwise, we'll meet build failure.
-	if [ $CLANG ]; then
-		do_build
-	else
-		${UEFI_TOOLS_DIR}/edk2-build.sh -b $BUILD_OPTION -a ../arm-trusted-firmware -s ../optee_os -e ../edk2 -p ../edk2-platforms -n ../edk2-non-osi -T gcc $PLATFORM
-	fi
-else
-	cd ${EDK2_DIR}
-	${UEFI_TOOLS_DIR}/uefi-build.sh -b $BUILD_OPTION -a ../arm-trusted-firmware -s ../optee_os $PLATFORM
-fi
+do_build
 if [ $? != 0 ]; then
 	echo "Fail to build UEFI & ARM Trusted Firmware ($?)"
 	exit
