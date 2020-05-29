fcc4eba2d9505c46064bd9515cb61ee2e2f11607
Victor Chong <victor.chong@linaro.org>
2020-05-29 09:33:58 +0100

    wip

--- hikey.mk
+++ hikey.mk
@@ -30,26 +30,29 @@ all: l-loader.bin prm_ptable.img recovery.bin
 %.o: %.S
 	$(CC) $(CFLAGS) -c -o $@ $<
 
-loader: start.o l-loader.lds
+ver:
+	clang --version
+
+loader: ver start.o l-loader.lds
 	$(LD) -Bstatic -Tl-loader.lds -Ttext 0xf9800800 start.o -o $@
 
 temp: loader
 	$(OBJCOPY) -O binary $< $@
 
-l-loader.bin: temp $(BL2)
+l-loader.bin: ver temp $(BL2)
 	python gen_loader_hikey.py -o $@ --img_loader=temp --img_bl1=$(BL2)
 
 .PHONY: check_sgdisk
 check_sgdisk:
 	@which sgdisk || (echo "sgdisk NOT found. Please install it!"; exit 1)
 
-prm_ptable.img: check_sgdisk
+prm_ptable.img: check_sgdisk ver
 	for ptable in $(PTABLE_LST); do \
 		PTABLE=$${ptable} SECTOR_SIZE=512 bash -x generate_ptable.sh;\
 		cp prm_ptable.img ptable-$${ptable}.img;\
 	done
 
-recovery.bin: temp $(BL1) $(NS_BL1U)
+recovery.bin: ver temp $(BL1) $(NS_BL1U)
 	python gen_loader_hikey.py -o $@ --img_loader=temp --img_bl1=$(BL1) --img_ns_bl1u=$(NS_BL1U)
 
 .PHONY: clean
